ff192fe26a1b21650d41ee9c7f1b4759a166fc84
Flechazo <FlechazoPh@foxmail.com>
2024-04-03 15:52:05 +0800

    Update messages.ts

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -1,22 +1,22 @@
 import axios from 'axios';
-
-async function sendTelegramMessage(botToken: string, chatId: string, text: string): Promise<void> {
+import runtime from "../runtime.ts";
+async function sendTelegramMessage(text: string): Promise<void> {
     try {
-        const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
+        const url = `https://api.telegram.org/bot${runtime.botToken}/sendMessage`;
         const params = {
-            chat_id: chatId,
+            chat_id: runtime.chatId,
             text: text
         };
         await axios.get(url, { params });
-        console.log('Message sent successfully');
+        console.log('TG Message sent successfully');
     } catch (error) {
-        console.error('Error sending message:', error);
+        console.error('Error sending TG message:', error);
     }
 }
 
 // 使用示例
 // const botToken = 'YOUR_BOT_TOKEN';
-// const chatId = '-636218921';
+// const chatId = '-1234';
 // const messageText = 'Hello World';
 
 // sendTelegramMessage(botToken, chatId, messageText);
